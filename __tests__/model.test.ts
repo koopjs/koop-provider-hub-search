ac15438c3b3a8edd080bee8f6ae139de045f586e
Andrew Tate <drewctate@gmail.com>
2021-07-19 09:01:51 -0500

    Flesh out model

---
+++ __tests__/model.test.ts
@@ -0,0 +1,80 @@
+import * as fetchMock from 'fetch-mock-jest';
+import * as faker from 'faker';
+import { Request } from 'express';
+import { HubApiModel } from '../src/model';
+import { PagingStream } from "../src/paging-stream";
+jest.mock('../src/paging-stream');
+
+describe('HubApiModel', () => {
+  // this is just to make the type checker happy
+  const MockedPagingStream = PagingStream as unknown as jest.Mock<PagingStream>;
+
+  it('configures and returns a paging stream', async () => {
+    const hubSiteUrl = faker.internet.url();
+
+    const model = new HubApiModel({
+      defaultSiteUrl: hubSiteUrl
+    });
+
+    const req = {
+      app: {
+        locals: {
+          searchOptions: {
+            query: faker.random.word()
+          }
+        }
+      }
+    } as unknown as Request;
+
+    const stream = model.getStream(req);
+
+    expect(stream).toBeInstanceOf(PagingStream);
+    expect(MockedPagingStream).toHaveBeenCalledTimes(1);
+
+    // Now, test the stuff that got passed to PagingStream
+    const {
+      firstPage,
+      loadPage,
+      streamPage,
+      getNextPage
+   } = MockedPagingStream.mock.calls[0][0];
+
+    // Test firstPage
+    const firstUrl = new URL(firstPage);
+    expect(firstUrl.origin).toBe(hubSiteUrl);
+    expect(firstUrl.searchParams.get('q')).toBe(req.app.locals.searchOptions.query);
+
+    // Test loadPage
+    // Set up fetch-mock
+
+    const pageUrl = faker.internet.url();
+    const fakeResponse = {
+      data: [
+        { id: faker.datatype.uuid() }
+      ]
+    };
+    fetchMock.mock(pageUrl, JSON.stringify(fakeResponse));
+    expect(await loadPage(pageUrl)).toEqual(fakeResponse);
+
+    // Test streamPage
+    const mockPage = {
+      data: [
+        { id: faker.datatype.uuid() },
+        { id: faker.datatype.uuid() },
+        { id: faker.datatype.uuid() }
+      ],
+      links: {
+        next: faker.internet.url()
+      }
+    };
+    const pushMock = jest.fn();
+    streamPage(mockPage, pushMock);
+
+    mockPage.data.forEach(
+      (dataset, i) => expect(pushMock).toHaveBeenNthCalledWith(i+1, dataset)
+    );
+
+    // Test getNextPage
+    expect(getNextPage(mockPage)).toBe(mockPage.links.next);
+  });
+});
--- src/model.ts
+++ src/model.ts
@@ -1,13 +1,30 @@
 import { Request } from 'express';
-import { Readable } from 'stream';
+import { PagingStream } from './paging-stream';
+import { ISearchApiOptions } from './types';
+import { getSearchQueryParams } from './get-search-query-params';
 
 export class HubApiModel {
-  async getStream (request: Request) {
-    console.log(request);
+  private _options: any;
 
-    const ret = new Readable({ read () {} });
-    ret.push({ foo: 'bar' });
-    ret.push(null);
-    return ret;
+  constructor (options: any) {
+    this._options = options;
+  }
+
+  getStream (request: Request) {
+    // TODO revisit how we get these search options
+    const searchOptions: ISearchApiOptions = request.app.locals.searchOptions;
+    const siteUrl = request.app.locals.hubSiteUrl || this._options.defaultSiteUrl;
+
+    const searchApiStream = new PagingStream({
+      firstPage: `${siteUrl}?${getSearchQueryParams(searchOptions)}`,
+
+      loadPage: (url: string) => fetch(url).then(res => res.json()),
+
+      streamPage: (response, push) => response.data.forEach(dataset => push(dataset)),
+
+      getNextPage: response => response.links.next
+    });
+
+    return searchApiStream;
   }
 }
