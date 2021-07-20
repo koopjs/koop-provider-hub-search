3c15c1795f0cab21ee1dde197ea36f75a44be171
Andrew Tate <drewctate@gmail.com>
2021-07-20 14:32:01 -0500

    Some more cleanup

--- jest.config.js
+++ jest.config.js
@@ -11,7 +11,6 @@ module.exports = {
     "json",
     "node",
   ],
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(ts|js)x?$',
   coverageDirectory: 'coverage',
   collectCoverageFrom: [
     'src/**/*.{ts,tsx,js,jsx}',
