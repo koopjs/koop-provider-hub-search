b8df619c055455c2b33ed730190fef34b6a5c410
Andrew Tate <drewctate@gmail.com>
2021-07-27 10:48:50 -0500

    Removing additional routes

--- example-app/src/index.js
+++ example-app/src/index.js
@@ -1,6 +1,5 @@
 const config = require('config')
 const Koop = require('koop')
-const routes = require('./routes')
 const plugins = require('./plugins')
 
 // initiate a koop app
@@ -9,13 +8,6 @@ const koop = new Koop()
 // register koop plugins
 plugins.forEach((plugin) => {
   koop.register(plugin.instance, plugin.options)
-})
-
-// add additional routes
-routes.forEach((route) => {
-  route.methods.forEach((method) => {
-    koop.server[method](route.path, route.handler)
-  })
 })
 
 // start the server
