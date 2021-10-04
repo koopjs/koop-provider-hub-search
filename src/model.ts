f410854b4e51065d1c0b525c62831f67f44ea323
drspacemanphd <markabrown87@gmail.com>
2021-10-04 08:15:57 -0400

    f/1773 initial

--- src/model.ts
+++ src/model.ts
@@ -6,9 +6,13 @@ import { PagingStream } from './paging-stream';
 import { IContentSearchRequest, IContentSearchResponse, searchContent } from '@esri/hub-search';
 
 export class HubApiModel {
+  private totalResults = 0;
+
+  getTotalResults () { return this.totalResults; }
 
   getStream (request: Request) {
     const searchRequest: IContentSearchRequest = request.res.locals.searchRequest;
+    const pagesPerBatch: number = request.res.locals.pagesPerBatch;
 
     if (!_.has(searchRequest, 'options.portal')) {
       _.set(searchRequest, 'options.portal', 'https://www.arcgis.com');
@@ -21,13 +25,20 @@ export class HubApiModel {
         if (typeof params === 'function') {
           return params();
         }
-
+        this.totalResults = 0;
         return searchContent(params); // first page request
       },
 
-      streamPage: (response, push) => response.results.forEach(result => push(result)),
+      streamPage: (response, push) => response.results.forEach(result => {
+        this.totalResults++;
+        push(result);
+      }),
 
-      getNextPageParams: response => response.hasNext && response.next
+      getNextPageParams: response => response.hasNext && response.next,
+
+      resetResults: () => this.totalResults = 0,
+
+      pagesPerBatch,
     });
 
     return searchApiStream;
--- src/paging-stream.ts
+++ src/paging-stream.ts
@@ -5,6 +5,8 @@ export interface PagingStreamOptions {
   loadPage: (request: any) => Promise<any>;
   streamPage: (response: any, push: typeof Readable.prototype.push) => any;
   getNextPageParams: (response: any) => any;
+  resetResults?: () => void;
+  pagesPerBatch?: number;
 }
 
 export class PagingStream extends Readable {
@@ -12,12 +14,20 @@ export class PagingStream extends Readable {
   private _loadPage;
   private _streamPage;
   private _getNextPageParams;
+  private _resetResults;
+  private _minPageTime;
+  public _getMinPageTime;
+  public _resetMinPageTime;
+  private _pagesPerBatch = Number.MAX_SAFE_INTEGER;
+  private _currPage = 0;
 
   constructor({
     firstPageParams,
     loadPage,
     streamPage,
-    getNextPageParams
+    getNextPageParams,
+    resetResults,
+    pagesPerBatch,
   }: PagingStreamOptions) {
     super({ objectMode: true });
 
@@ -25,22 +35,51 @@ export class PagingStream extends Readable {
     this._loadPage = loadPage;
     this._streamPage = streamPage;
     this._getNextPageParams = getNextPageParams;
+    this._resetResults = resetResults;
+    this._minPageTime = Number.MAX_SAFE_INTEGER;
+    this._getMinPageTime = () => this._minPageTime;
+    this._resetMinPageTime = () => {
+      this._minPageTime = Number.MAX_SAFE_INTEGER;
+    };
+    this._pagesPerBatch = pagesPerBatch;
   }
 
   async _read () {
     let response: any;
+    const start = new Date().getTime();
+
     try {
       response = await this._loadPage(this._nextPageParams);
+      this._currPage++;
     } catch (err) {
+      this._resetResults();
+      this._resetMinPageTime();
       this.destroy(err);
       return;
     }
 
+    const end = new Date().getTime();
+
+    if (!this._nextPageParams.apply) {
+      console.log(`FETCHED PAGE ${this._currPage} WITH KEY ${this._nextPageParams.options?.page}`);
+    } else {
+      const query = JSON.parse(response.query);
+      const key = query?.page?.key || '';
+      const buffer = Buffer.from(key, 'base64');
+      console.log(`FETCHED PAGE ${this._currPage} WITH KEY ${buffer.toString('utf-8')}`);
+    }
+
     this._nextPageParams = this._getNextPageParams(response);
+
+    if (this._nextPageParams) {
+      if (end - start < this._minPageTime) {
+        this._minPageTime = end - start;
+      }
+    }
 
     this._streamPage(response, this.push.bind(this));
 
-    if (!this._nextPageParams) {
+    if (!this._nextPageParams || this._currPage >= this._pagesPerBatch) {
       this.push(null);
     }
   }
